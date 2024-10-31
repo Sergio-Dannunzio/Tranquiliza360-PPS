1870ec1a92660baad849b5de3153ab0466952089
Héctor Bruña <hectorbct.2002@gmail.com>
2024-10-31 13:40:39 -0300

    router y navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
