9525f8c23fdb925e6bbbc759c25f532e17d720bb
[Sergio-Dannunzio] <sergiodannunzio@hotmail.com>
2024-11-07 09:13:29 -0300

    fix responsive

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,62 +1,55 @@
 import React from "react";
 import hero from "../assets/heroImg.png";
-import { FaYoutube } from "react-icons/fa";
 import { IoIosMail } from "react-icons/io";
-import { FaLinkedin } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
 import { IoMdArrowDropdown } from "react-icons/io";
 
 const Login = () => {
   return (
     <div
-      className="bg-cover bg-center w-screen  h-screen flex justify-between "
+      className="bg-cover bg-center w-screen  h-screen flex flex-col justify-between xl:flex-row"
       style={{ backgroundImage: `url(${hero})` }}
     >
-      <div className="ml-56 mb-56 flex flex-col justify-center">
-        <h1 className="mb-8 text-6xl text-[#2D5677] font-bold">
-          Bienvenido a Pladema
+      <div className="pt-20 xl:ml-56 xl:mb-56 flex flex-col justify-center items-center xl:items-start">
+        <h1 className="mb-8 text-4xl md:text-6xl text-[#2D5677] font-bold text-center md:px-4">
+          Bienvenido a Tranquiliza 360°
         </h1>
-        <form action="" className="flex flex-col justify-center">
+        <form action="" className="flex flex-col justify-center w-full px-4 md:w-4/6">
           <input
             type="text"
             name=""
             id=""
             placeholder="Ingresa tu correo electronico"
-            className="w-[32rem] mt-2 mb-2 h-10 placeholder:pl-2"
+            className="xl:w-[32rem] mt-2 mb-2 h-10 placeholder:pl-2"
           />
-          <div className="flex">
+          <div className="flex  xl:w-[32rem]">
             <input
               type="text"
               name=""
               id=""
               placeholder="Ingresa tu contraseña"
-              className="w-96 mt-2 mb-2 h-10 placeholder:pl-2"
+              className="flex-grow xl:w-96 mt-2 mb-2 h-10 placeholder:pl-2"
             />
-            <button className="w-28 bg-[#2D5677] h-10 mt-2 mb-2 ml-4 text-[#ffffff]">
+            <button className="xl:w-28 bg-[#2D5677] h-10 mt-2 mb-2 ml-4 px-4 text-[#ffffff]">
               ENTRAR
             </button>
           </div>
-        </form>
-        <div className="flex flex-col mb-2 mt-2">
-          <a href="" className="font-bold mb-2 w-52">
+        <div className="flex flex-col mb-2 mt-2 w-full">
+          <a href="" className="font-bold mb-2 text-center w-full xl:text-left">
             ¿Olvidaste tu contraseña?
           </a>
-          <a href="/register" className="font-bold w-56">
+          <a href="/register" className="font-bold text-center w-full xl:text-left">
             ¿No tienes cuenta? Registrate
           </a>
         </div>
+        </form>
       </div>
-      <div className="mr-6 flex flex-col justify-between items-center">
+      <div className="xl:mr-6 flex flex-col justify-between items-center">
         <div className="flex justify-center items-center mt-2">
           <p className="self-start mr-1">Español</p>
           <IoMdArrowDropdown />
         </div>
-        <div className="flex flex-col items-center mt-auto mb-auto space-y-4 pb-56">
-          <a
-            href=""
-            className="bg-[#000000] rounded-full p-2 w-12 h-12 justify-center flex items-center"
-          >
-            <FaLinkedin className="text-white text-2xl" />
-          </a>
+        <div className="flex xl:flex-col items-center xl:mt-auto xl:mb-auto xl:space-y-4 space-x-4 xl:space-x-0 pb-4 xl:pb-56">
           <a
             href=""
             className="bg-[#000000] rounded-full p-2 w-12 h-12 justify-center flex items-center"
@@ -67,7 +60,7 @@ const Login = () => {
             href=""
             className="bg-[#000000] rounded-full p-2 w-12 h-12 justify-center flex items-center"
           >
-            <FaYoutube className="text-white text-2xl" />
+            <FaInstagram  className="text-white text-2xl"/>
           </a>
         </div>
       </div>
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,50 +1,56 @@
 import React from "react";
 import hero from "../assets/heroImg.png";
-import { FaYoutube } from "react-icons/fa";
 import { IoIosMail } from "react-icons/io";
-import { FaLinkedin } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
 import { IoMdArrowDropdown } from "react-icons/io";
 
 const Register = () => {
-  return <div className="bg-cover bg-center w-screen h-screen flex justify-between "
+  return <div className="bg-cover bg-center w-screen  h-screen flex flex-col justify-between xl:flex-row"
   style={{ backgroundImage: `url(${hero})` }}>
-    <div className="ml-56 mb-56 flex flex-col justify-center">
-      <h1 className="mb-8 text-6xl text-[#2D5677] font-bold">Registro en Pladema</h1>
-      <form action="" className="flex flex-col justify-center">
-        <div className="flex">
-          <input type="text" name="" id="" placeholder="Ingresa tu nombre y apellido" className="w-[20rem] pl-2 mt-2 mb-2 mr-2 h-10"/>
-          <input type="text" name="" id="" placeholder="Ingresa tu correo electronico" className="w-[20rem] pl-2 mt-2 mb-2 ml-2 h-10"/>
+    <div className="pt-20 xl:ml-56 xl:mb-56 flex flex-col justify-center items-center xl:items-start">
+      <h1 className="mb-8 text-4xl md:text-6xl text-[#2D5677] font-bold text-center md:px-4">Registro en Tranquiliza 360°</h1>
+      <form action="" className="flex flex-col justify-center w-full px-4 md:w-4/6">
+        <div className="flex flex-col xl:flex-row">
+          <input type="text" name="" id="" placeholder="Ingresa tu nombre y apellido" className="w-full flex-grow xl:w-1/2 pl-2 mt-2 mb-2 xl:mr-2 h-10"/>
+          <input type="text" name="" id="" placeholder="Ingresa tu correo electronico" className="w-full flex-grow xl:w-1/2 pl-2 mt-2 mb-2 xl:ml-2 h-10"/>
         </div>
-        <textarea placeholder="Ingresa tus lugares favoritos" className="w-[41rem] mt-2 mb-2 h-32 p-2 text-left resize-none"/>
+        <textarea placeholder="Ingresa tus lugares favoritos" className="xl:w-full mt-2 mb-2 h-32 p-2 text-left resize-none"/>
         
-        <div className="flex">
-          <input type="text" name="" id="" placeholder="Ingresa tu contraseña" className="w-64 pl-2 mt-2 mb-2 mr-2 h-10"/>
-          <input type="text" name="" id="" placeholder="Repite tu contraseña" className="w-64 pl-2 mt-2 mb-2 ml-2 h-10"/>
-          <button className="w-28 bg-[#2D5677] h-10 mt-2 mb-2 ml-4 text-[#ffffff]">
-            ENTRAR
-          </button>
+        <div className="flex flex-col xl:flex-row">
+          <div className="xl:w-2/6 flex-grow xl:mr-4">
+            <input type="text" name="" id="" placeholder="Ingresa tu contraseña" className="flex-grow w-full pl-2 mt-2 mb-2 h-10"/>
+          </div>
+          <div className="flex">
+            <input type="text" name="" id="" placeholder="Repite tu contraseña" className="flex-grow w-4/6 xl:w-1/2 pl-2 mt-2 mb-2 h-10"/>
+            <a href="/" className="h-12">
+              <button className="xl:w-28 bg-[#2D5677] h-10 mt-2 mb-2 ml-4 px-4 text-[#ffffff]">ENTRAR</button>
+            </a>
+          </div>
         </div>
+      <div className="flex flex-col mb-2 mt-2 items-center md:items-start">
+        <a href="/login" className="font-bold w-56 text-center md:text-left">¿Ya tienes cuenta? Logueate</a>
+      </div>
       </form>
-      <div className="flex flex-col mb-2 mt-2">
-        <a href="/login" className="font-bold w-56">¿Ya tienes cuenta? Logueate</a>
-      </div>
     </div>
-    <div className="mr-6 flex flex-col justify-between items-center">
-      <div className="flex justify-center items-center mt-2">
-        <p className="self-start mr-1">Español</p>
-        <IoMdArrowDropdown />
-      </div>
-      <div className="flex flex-col items-center mt-auto mb-auto space-y-4 pb-56">
-        <a href="" className="bg-[#000000] rounded-full p-2 w-12 h-12 justify-center flex items-center">
-          <FaLinkedin className="text-white text-2xl"/>
-        </a>
-        <a href="" className="bg-[#000000] rounded-full p-2 w-12 h-12 justify-center flex items-center">
-          <IoIosMail className="text-white text-2xl"/>
-        </a>
-        <a href="" className="bg-[#000000] rounded-full p-2 w-12 h-12 justify-center flex items-center">
-          <FaYoutube className="text-white text-2xl"/>
-        </a>
-      </div>
+    <div className="xl:mr-6 flex flex-col justify-between items-center">
+        <div className="flex justify-center items-center mt-2">
+          <p className="self-start mr-1">Español</p>
+          <IoMdArrowDropdown />
+        </div>
+        <div className="flex xl:flex-col items-center xl:mt-auto xl:mb-auto xl:space-y-4 space-x-4 xl:space-x-0 pb-4 xl:pb-56">
+          <a
+            href=""
+            className="bg-[#000000] rounded-full p-2 w-12 h-12 justify-center flex items-center"
+          >
+            <IoIosMail className="text-white text-2xl" />
+          </a>
+          <a
+            href=""
+            className="bg-[#000000] rounded-full p-2 w-12 h-12 justify-center flex items-center"
+          >
+            <FaInstagram  className="text-white text-2xl"/>
+          </a>
+        </div>
     </div>
   </div>;
 };
