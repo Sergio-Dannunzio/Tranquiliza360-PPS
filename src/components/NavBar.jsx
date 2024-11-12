8dbb362e5f75a885da6c5db4d0fce39a0214f506
[Sergio-Dannunzio] <sergiodannunzio@hotmail.com>
2024-11-12 08:00:06 -0300

    agrego algunas funciones

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,13 @@
 import { NavLink } from "react-router-dom";
 import plademaimglogo from "../assets/pladema.png";
+
 const NavBar = () => {
+
   return (
     <div className="container mx-[25%] w-1/2 flex justify-between absolute items-center bg-white rounded-full shadow-2xl p-2 m-4">
-      <div>
+      <a href="https://www.pladema.net/" className="cursor-pointer">
         <img src={plademaimglogo} className="h-10" alt="plademalogo" />
-      </div>
+      </a>
       <NavLink to="/" className="">
         Home
       </NavLink>
@@ -16,7 +18,7 @@ const NavBar = () => {
         Blogs
       </NavLink>
       <NavLink to="/login" className="">
-        iniciar sesión
+        Iniciar sesión
       </NavLink>
     </div>
   );
--- src/pages/Blogs.jsx
+++ src/pages/Blogs.jsx
@@ -1,17 +1,25 @@
 import BlogCard from "../components/BlogCard";
 import blogs from "../utils/BlogData"
+import { useNavigate } from 'react-router-dom';
 
 const Blogs = () => {
+
+  const navigate = useNavigate();
+
+  const handleShowDetails = (num) => {
+    navigate(`/blog/${num}`);
+  };
+
   return (
     <div className="w-full flex flex-col justify-center">
-      <div className="flex flex-col cursor-pointer mt-32 mx-2 relative md:mx-10 lg:mx-52 xl:mx-64">
+      <div onClick={() => handleShowDetails(blogs[0].id)} className="flex flex-col cursor-pointer mt-32 mx-2 relative md:mx-10 lg:mx-52 xl:mx-64">
         <img src="/assets/rv_convocatoria_memoria.png" alt=""  className=" 2xl:h-[750px] object-fit"/>
-        <h1 className="text-[#ffffff] absolute bottom-8 mx-2 text-center">[TANDIL] ¡Te necesitamos! Buscamos voluntarios para participar de una investigación en un entorno de realidad virtual.</h1>
-        <p className="text-sm text-[#1C374D] mx-2">16/17/18/19 de Noviembre de 2024</p>
+        <h1 className="text-[#ffffff] absolute bottom-8 mx-2 text-center">{blogs[0].title}</h1>
+        <p className="text-sm text-[#1C374D] mx-2">{blogs[0].date}</p>
       </div>
 
-      <div className="flex flex-col xl:flex-row justify-center items-center mt-24">
-        {blogs.map((blog) => (
+      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 justify-center items-center mt-24 xl:mx-auto">
+        {blogs.slice(1).map((blog) => (
           <BlogCard img={"/rv_convocatoria_memoria.png"} title={blog.title} date={blog.date} key={blog.id} id={blog.id}/>
         ))}
       </div>
